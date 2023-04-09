80ea68ca6fd93195fe8971eb83e7f12a637f76c0
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2023-04-09 13:12:43 -0300

    added another solution for roman to integer problem

--- 13-roman-to-integer/solution.js
+++ 13-roman-to-integer/solution.js
@@ -43,17 +43,34 @@
 //   return result;
 // };
 
+// const romanToInt = (s) => {
+//   const values = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
+//     'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900
+//   };
+
+//   let result = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     const value = values[`${s[i]}${s[i + 1]}`];
+
+//     if (value) {
+//       result += value;
+//       i++;
+//       continue;
+//     }
+
+//     result += values[s[i]];
+//   }
+
+//   return result;
+// };
+
 const romanToInt = (s) => {
-  const values = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
-    'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900
-  };
+  const values = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 };
 
   let result = 0;
   for (let i = 0; i < s.length; i++) {
-    const value = values[`${s[i]}${s[i + 1]}`];
-
-    if (value) {
-      result += value;
+    if (values[s[i]] < values[s[i + 1]]) {
+      result += values[s[i + 1]] - values[s[i]];
       i++;
       continue;
     }
