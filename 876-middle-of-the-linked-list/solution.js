667d94c78d7e69eea7e1484b90426ae76b95d183
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2023-04-17 21:04:36 -0300

    added solution for middle of the linked list problem

---
+++ 876-middle-of-the-linked-list/solution.js
@@ -0,0 +1,38 @@
+/*
+  Given the head of a singly linked list, return the middle node of the linked
+  list.
+
+  If there are two middle nodes, return the second middle node.
+
+  Definition for singly-linked list:
+
+  function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+  }
+*/
+
+function ListNode(val, next) {
+  this.val = (val===undefined ? 0 : val)
+  this.next = (next===undefined ? null : next)
+};
+
+const middleNode = (head) => {
+  let temp = head, size = 0;
+  while (temp) {
+    temp = temp.next;
+    size++;
+  }
+
+  let middleIndex = Math.floor(size / 2);
+
+  let middleNode = head;
+  while (middleIndex > 0) {
+    middleNode = middleNode.next;
+    middleIndex--;
+  }
+
+  return middleNode;
+};
+
+module.exports = middleNode;
