36687a6773b4a5062df7b9089b39d18bd3e43d18
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2023-05-04 21:37:21 -0300

    adjusted problem description

--- 3-longest-substring-without-repeating-characters/solution.js
+++ 3-longest-substring-without-repeating-characters/solution.js
@@ -1,5 +1,6 @@
 /*
-  Given a string s, find the length of the longest substring without repeating characters.
+  Given a string s, find the length of the longest substring without repeating
+  characters.
 */
 
 const lengthOfLongestSubstring = (s) => {
